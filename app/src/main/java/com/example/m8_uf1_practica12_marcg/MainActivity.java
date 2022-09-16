d076e508f8f2ed0fea8a1fcc0e424c6a0b6306bd
MarcG <marcgalaxy02@gmail.com>
2022-09-16 19:27:32 +0200

    Commit2

--- app/src/main/java/com/example/m8_uf1_practica12_marcg/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_practica12_marcg/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.m8_uf1_practica12_marcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int number = 22;
+        TextView input = (TextView) findViewById(R.id.numberU);
+
+        Button button = (Button) findViewById(R.id.verify);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CharSequence text = "";
+                int duration = Toast.LENGTH_SHORT;
+                int valueUser = Integer.valueOf(input.getText().toString());
+                // Do something in response to button click
+                Context context = getApplicationContext();
+                if (valueUser == number){
+                    text = "Nice!! " + valueUser + " is the correct number!!";
+                }
+                else if (valueUser < number){
+                    text = "Wrong!! " + valueUser + " is smaller than the number!!";
+                }
+                else{
+                    text = "Wrong!! " + valueUser + " is biger than the number!!";
+                }
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
     }
 }
