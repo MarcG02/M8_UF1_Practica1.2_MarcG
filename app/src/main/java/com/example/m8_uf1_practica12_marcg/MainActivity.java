9ca1e7b8e159068c6720dffb90aa9869406495b6
MarcG <marcgalaxy02@gmail.com>
2022-09-23 19:10:59 +0200

    Commit2

--- app/src/main/java/com/example/m8_uf1_practica12_marcg/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_practica12_marcg/MainActivity.java
@@ -15,25 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int number = 22;
+        int number = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
         TextView input = (TextView) findViewById(R.id.numberU);
+        TextView log = (TextView) findViewById(R.id.Registry);
 
         Button button = (Button) findViewById(R.id.verify);
         button.setOnClickListener(new View.OnClickListener() {
+            int userTry = 0;
             public void onClick(View v) {
                 CharSequence text = "";
+                userTry++;
+
                 int duration = Toast.LENGTH_SHORT;
                 int valueUser = Integer.valueOf(input.getText().toString());
+
                 // Do something in response to button click
                 Context context = getApplicationContext();
                 if (valueUser == number){
                     text = "Nice!! " + valueUser + " is the correct number!!";
+                    log.append("Nice!! " + valueUser + " is the correct number!! Tries = " + userTry + "\n");
                 }
                 else if (valueUser < number){
                     text = "Wrong!! " + valueUser + " is smaller than the number!!";
+                    log.append("Wrong!! " + valueUser + " is smaller than the number!! Tries = " + userTry + "\n");
                 }
                 else{
                     text = "Wrong!! " + valueUser + " is biger than the number!!";
+                    log.append("Wrong!! " + valueUser + " is biger than the number!!\nTries = " + userTry + "\n");
                 }
 
                 Toast toast = Toast.makeText(context, text, duration);
